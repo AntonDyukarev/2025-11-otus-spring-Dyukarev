0e85316f9e1e62242f4024bfd04ddc403b1b89bc
Anton Dyukarev <adukarev@vtb.ru>
2025-12-10 03:27:21 +0300

    try with resources for input stream

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -7,6 +7,7 @@ import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
@@ -21,21 +22,23 @@ public class CsvQuestionDao implements QuestionDao {
     public List<Question> findAll() {
         String questionsFileName = fileNameProvider.getTestFileName();
 
-        InputStream inputStream = getClass().getResourceAsStream("/" + questionsFileName);
-        if (Objects.isNull(inputStream)) {
-            String errorMessage = String.format("Could not find file with name %s", questionsFileName);
-            throw new QuestionReadException(errorMessage);
+        try (InputStream inputStream = getClass().getResourceAsStream("/" + questionsFileName);) {
+            if (Objects.isNull(inputStream)) {
+                String errorMessage = String.format("Could not find file with name %s", questionsFileName);
+                throw new QuestionReadException(errorMessage);
+            }
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            return new CsvToBeanBuilder<QuestionDto>(inputStreamReader)
+                    .withSkipLines(1)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .build()
+                    .parse()
+                    .stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-
-        return new CsvToBeanBuilder<QuestionDto>(inputStreamReader)
-                .withSkipLines(1)
-                .withType(QuestionDto.class)
-                .withSeparator(';')
-                .build()
-                .parse()
-                .stream()
-                .map(QuestionDto::toDomainObject)
-                .toList();
     }
 }
